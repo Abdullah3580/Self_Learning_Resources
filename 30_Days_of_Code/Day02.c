bbad8950d090c16c24c7bda3f24d8ecedb91e439
Md Abdullah <mdabdullahalsiam358@gmail.com>
2026-01-31 21:30:24 +0600

    ....

---
+++ 30_Days_of_Code/Day02.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int i;
+    double d;
+    string s[100];
+    scanf("%d",&i);
+    scanf("%f",&d);
+    getchar();
+    fgets(s,100,stdin);
+    printf("%d",i+d);
+    printf("%.f",d+d);
+    s = ""
+
+}
--- C/test.c
+++ C/test.c
@@ -1,6 +1,37 @@
 #include<stdio.h>
-int main(){
-    int s;
-    scanf("%d",&s);
-    printf("your age is: ",s);
+int sum = 0;
+int n;
+int i=0;
+
+int main()
+{
+    //printf("input n: ");
+    scanf("%d",&n);
+    int arr[n];
+    char s[100];
+    getchar();
+    fgets(s,100,stdin);
+
+    int j = 0;
+    while(s[i] != '\0')
+    {
+        printf("i:%d ",i);
+        if(s[i] >= '0' && s[i]<= '9' && j < 3)
+        {
+            int d= (int)s[i] - '0';
+            arr[j] = d;
+            printf("j:%d %d ",j,arr[j]);
+            j++;
+            sum = sum + d;
+        }
+        printf("\n");
+        i++;
+    }
+    //for(int i =0 ;i < n; i++)
+    //{
+        //printf("%d ",arr[j]);
+        //printf("%d ",arr[i]);
+   // }
 }
+
+
